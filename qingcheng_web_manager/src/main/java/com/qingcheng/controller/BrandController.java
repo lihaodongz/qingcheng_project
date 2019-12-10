3bfe02f988bae415cf8e12026d06d4c431e8db5c
lihaodongz <7991932112qq.com>
2019-12-10 20:38:56 +0800

    brand 的CURD以及测试

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/BrandController.java
@@ -1,12 +1,15 @@
 package com.qingcheng.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.qingcheng.pojo.entity.PageResult;
+import com.qingcheng.pojo.entity.Result;
 import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/brand")
@@ -18,5 +21,40 @@ public class BrandController {
     @RequestMapping("findAll")
     public List<Brand> findAll(){
         return  brandService.findAll();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult<Brand> findPage(int page,int size){
+        return brandService.findPage(page,size);
+    }
+    @PostMapping("/findList")
+    public List<Brand> findList(@RequestBody  Map searchMap){
+        return brandService.findList(searchMap);
+    }
+
+    @PostMapping("/findPage")
+    public PageResult<Brand> findPage(@RequestBody  Map searchMap,int page,int size){
+        return brandService.findPage(searchMap,page,size);
+    }
+
+    @GetMapping("/findById")
+    public Brand findById(Integer id){
+        return brandService.findById(id);
+    }
+    @PostMapping("/add")
+    public Result add(@RequestBody  Brand brand){
+         brandService.add(brand);
+         return  new Result();
+    }
+
+    @PostMapping("/update")
+    public Result update(@RequestBody Brand brand){
+        brandService.update(brand);
+        return  new Result();
+    }
+    @GetMapping("/delete")
+    public Result delete(Integer id){
+        brandService.deleteById(id);
+        return new Result();
     }
 }
