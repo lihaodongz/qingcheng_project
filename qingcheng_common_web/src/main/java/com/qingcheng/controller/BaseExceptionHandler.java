64c299a8d945cd4ee03cfb58541623f4d16463e6
lihaodongz <7991932112qq.com>
2019-12-14 13:18:08 +0800

    代码结构优化，以及模块建立完成

--- qingcheng_common_web/src/main/java/com/qingcheng/controller/BaseExceptionHandler.java
+++ qingcheng_common_web/src/main/java/com/qingcheng/controller/BaseExceptionHandler.java
@@ -1,19 +1,23 @@
 package com.qingcheng.controller;
 
-import com.qingcheng.pojo.entity.Result;
+import com.qingcheng.entity.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+/**
+ * 统一异常处理类
+ */
 @ControllerAdvice
-public class BaseExceptionHandler  {
+public class BaseExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public Result error(Exception e){
+    public Result error(Exception e) {
         e.printStackTrace();
-        System.out.println("");
+        System.out.println("调用了公共异常处理类");
         return new Result(1,e.getMessage());
     }
+
+
 }
--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
@@ -1,15 +1,13 @@
 package com.qingcheng.controller.goods;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.qingcheng.pojo.entity.PageResult;
-import com.qingcheng.pojo.entity.Result;
+import com.qingcheng.entity.PageResult;
+import com.qingcheng.entity.Result;
 import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/brand")
@@ -18,42 +16,48 @@ public class BrandController {
     @Reference
     private BrandService brandService;
 
-    @RequestMapping("findAll")
+    @GetMapping("/findAll")
     public List<Brand> findAll(){
-        return  brandService.findAll();
+        return brandService.findAll();
     }
 
     @GetMapping("/findPage")
-    public PageResult<Brand> findPage(int page,int size){
-        return brandService.findPage(page,size);
+    public PageResult<Brand> findPage(int page, int size){
+        return brandService.findPage(page, size);
     }
+
     @PostMapping("/findList")
-    public List<Brand> findList(@RequestBody  Map searchMap){
+    public List<Brand> findList(@RequestBody Map<String,Object> searchMap){
         return brandService.findList(searchMap);
     }
 
     @PostMapping("/findPage")
-    public PageResult<Brand> findPage(@RequestBody  Map searchMap,int page,int size){
-        return brandService.findPage(searchMap,page,size);
+    public PageResult<Brand> findPage(@RequestBody Map<String,Object> searchMap,int page, int size){
+        return  brandService.findPage(searchMap,page,size);
     }
 
     @GetMapping("/findById")
     public Brand findById(Integer id){
         return brandService.findById(id);
     }
+
+
     @PostMapping("/add")
-    public Result add(@RequestBody  Brand brand){
-         brandService.add(brand);
-         return  new Result();
+    public Result add(@RequestBody Brand brand){
+        brandService.add(brand);
+        return new Result();
     }
+
     @PostMapping("/update")
     public Result update(@RequestBody Brand brand){
         brandService.update(brand);
-        return  new Result();
-    }
-    @GetMapping("/delete")
-    public Result delete(Integer id){
-        brandService.deleteById(id);
         return new Result();
     }
+
+    @GetMapping("/delete")
+    public Result delete(Integer id){
+        brandService.delete(id);
+        return new Result();
+    }
+
 }
